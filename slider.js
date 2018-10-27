65b1452d6e64d02461200a6e1a28dabb2534896e
Erik <32487247+erhed@users.noreply.github.com>
2018-10-27 16:38:40 +0200

    'antal personer' namnbyte

--- slider.js
+++ slider.js
@@ -1,5 +1,5 @@
 var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
+var output = document.getElementById("antalPersoner");
 output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
